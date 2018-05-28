6348f5cd9846d4744468a90979ff474ce2a9d48f
vuong <vuonganhtuyet102@gmail.com>
2018-05-28 19:40:29 +0900

    Test input matrix

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 EVENT_TYPE = ("click", "long-click", "menu", "back", "scroll", "network", "battery", "bluetooth", "swipe", "drag", "scale")
 GUI_TYPE = ("button", "menu-button", "back-button",  "input-text", "list-element")
 GUI_CLASS = ("android.widget.TextView", "android.widget.ImageView", "android.widget.FrameLayout", "android.widget.ListView")
-DEVICE = "emulator-5554"
+# DEVICE = "GAAZCY07T304WGP"
+DEVICE= "emulator-5554"
--- src/executor/executor.py
+++ src/executor/executor.py
@@ -1,27 +1,30 @@
-from config import *
+from config import EVENT_TYPE
 import re
+
+
+def get_bound_from_string(bound_string):
+    coordinate = re.findall(r'\d+', bound_string)
+    left = int(coordinate[0])
+    top = int(coordinate[1])
+    right = int(coordinate[2])
+    bottom = int(coordinate[3])
+    return [left, right, top, bottom]
+
+
+def get_click_point_from_bound(bound):
+    return ((bound[0] + bound[1]) / 2, (bound[2] + bound[3]) / 2)
+
+
+def get_scroll_point_from_bound(bound):
+    horizontal_center = (bound[0] + bound[1]) / 2
+    return horizontal_center, bound[2], horizontal_center, bound[3]
+
 
 class Executor(object):
     device = None
 
     def __init__(self, device):
         self.device = device
-
-    def get_bound_from_string(self, bound_string):
-        coordinate = re.findall(r'\d+', bound_string)
-        left = int(coordinate[0])
-        top = int(coordinate[1])
-        right = int(coordinate[2])
-        bottom = int(coordinate[3])
-        return [left, right, top, bottom]
-
-    def get_click_point_from_bound(self, bound):
-        return ((bound[0] + bound[1])/2, (bound[2] + bound[3])/2)
-
-    def get_scroll_point_from_bound(self, bound):
-        horizontal_center = (bound[0] + bound[1])/2
-        return horizontal_center, bound[2], horizontal_center, bound[3]
-
 
     def perform_action(self, component):
         """
@@ -30,7 +33,7 @@ class Executor(object):
         :param component:
         :return:
         """
-        bound = self.get_bound_from_string(component[2])
+        bound = get_bound_from_string(component[2])
         if component[1] not in EVENT_TYPE:
             return None
         else:
@@ -39,7 +42,7 @@ class Executor(object):
             elif component[1] == "long-click":
                 self.device.long_click((bound[0] + bound[1])/2, (bound[2] + bound[3])/2)
             elif component[1] == "scroll":
-                self.device.drag(self.get_scroll_point_from_bound(bound))
+                self.device.drag(get_scroll_point_from_bound(bound))
             elif component[1] == "text":
                 pass
 
